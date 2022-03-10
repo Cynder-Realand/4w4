367e35ef286695baa467b5ee137fa8374d610a18
Kyndra Dess <e1822062@cmaisonneuve.qc.ca>
2022-03-10 16:55:53 -0500

    S7-c2 Kyky sidebar-footer custom-logo

--- footer.php
+++ footer.php
@@ -1,10 +1,42 @@
 <footer class="site__footer">
-    <h2 class="footer__titre">Le beau footer</h2>
-    <p class="footer__presentation">Un site web fait par une élève du TIM à l'aide de Wordpress.</p>
-    <h3 class="footer__author">Fait par Kynndra Dess</h3>
-<?php 
+    <!----première rangée de colonne---->
+    <div class="site__footer__colonne">
+        <section class="footer__article">
+            <h2>Titre 1</h2>
+            <?php get_sidebar('footer_colonne_1'); ?>
+        </section>
+        <section class="footer__adresse">
+            <h2>Titre 2</h2>
+            <?php get_sidebar('footer_colonne_2'); ?>
+        </section>
+        <section class="footer__liens">
+            <h2>Titre 3</h2>
+            <?php get_sidebar('footer_colonne_3'); ?>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil architecto ea nobis. Laborum cum voluptas
+                quibusdam voluptatibus dolores tempora fugiat iste exercitationem nihil, excepturi dolorem veritatis
+                officia perspiciatis quidem harum!</p>
+                <?php wp_nav_menu(array(
+                "menu" => "externe")) ?>
+        </section>
+    </div>
+    <!----rangée de plusieurs lignes---->
+    <div class="site__footer__ligne">
+        <section class="footer__description"></section>
+        <section class="footer__nav"></section>
+        <section class="footer__sociaux">
+        <?php get_sidebar('footer_ligne_1'); ?>
+        </section>
+        <section class="footer__copyright"></section>
+        <section class="footer__auteur"></section>
+    </div>
 
-$icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#f00"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
+
+    <h2 class="footer__titre">Yes, indeed, I am a Sukuna simp</h2>
+    <p class="footer__presentation">Site web réalisé dans le cadre du cours 4W4</p>
+    <h3 class="footer__author">Réalisé par Kyndra Dess</h3>
+    <?php 
+
+$icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000000"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd"></path></svg>';
 wp_nav_menu(array(
                     "menu"=>"simple",
                     "container"=>"nav",
@@ -12,7 +44,7 @@ wp_nav_menu(array(
                     "menu_class"=>"site__footer__menu__ul",
 
                     "link_before"=>$icone)); ?>
-                    <?php get_search_form(); ?>
+    <div class="footer__recherche"><?php get_search_form(); ?></div>
 </footer>
 <?php wp_footer(); ?>
 </body>
